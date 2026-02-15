50742ade7abcf430aeb417fe01af3956b51328bb
JoGeorges <141683247+JoGeorges@users.noreply.github.com>
2026-02-15 17:09:23 -0500

    Initialize Flutter app with Home and Contact screens

---
+++ session2/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) {
+    return MaterialApp (
+      home: HomeScreen()
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('ZapZapp'),
+      leading: Icon(Icons.menu),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.settings),
+          onPressed: () {
+            Navigator.push(context,
+              MaterialPageRoute(builder: (context) => ContactScreen())
+            );
+          }
+        ), 
+        IconButton(
+          icon: Icon(Icons.more_vert),
+          onPressed: () {
+            print("Ou peze bouton an");
+          },
+        ), 
+      ], 
+    ),
+    body: Container(
+      padding: EdgeInsets.all(10.0),
+      child: ElevatedButton(
+        child: Text("Go to ContactScreen"),
+        onPressed: () {
+          Navigator.push(context,
+            MaterialPageRoute(builder: (context) => ContactScreen())
+          );
+        }
+      ),
+    ),
+    bottomNavigationBar: BottomNavigationBar(
+      currentIndex: 1,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.checklist),
+          label: "Check List",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.contact_support),
+          label: "Contact",
+        ),
+      ],
+      onTap: (index){
+        if (index == 2){
+          Navigator.push(context, 
+            MaterialPageRoute(builder: (context) => ContactScreen())
+          );
+        } else {
+          print("Index ki peze a se: $index");
+        }
+      }
+    ),
+  );
+  }
+}
+
+
+
+class ContactScreen extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('ZapZapp'),
+      leading: Icon(Icons.menu),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.settings),
+          onPressed: () {
+            Navigator.push(context,
+              MaterialPageRoute(builder: (context) => ContactScreen())
+            );
+          }
+        ), 
+        IconButton(
+          icon: Icon(Icons.more_vert),
+          onPressed: () {
+
+          },
+        ), 
+      ], 
+    ),
+    body: Container(
+      
+    ),
+    bottomNavigationBar: BottomNavigationBar(
+      items: [
+
+      ]
+    ),
+  );
+  }
+}
